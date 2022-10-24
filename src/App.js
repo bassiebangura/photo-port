af1470cc77a0f617df5904ca032163f9e2cd834b
BassieBangura <devbassiebangura@gmail.com>
2022-10-24 04:32:02 -0500

    Add navbar. Add css

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import './App.css';
+import About from './components/about';
+import Nav from './components/nav';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-       <p>Hello And Welcome To 2u Coding Bootcamp!!!!</p>
-      </header>
+    <div>
+      <Nav />
+      <main>
+        <About />
+      </main>
     </div>
   );
 }
